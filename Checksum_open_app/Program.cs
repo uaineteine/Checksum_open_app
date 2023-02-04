2640756e2e555b44685e48273dc4341da3b76d7b
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-02-05 10:21:08 +1100

    Add project files.

---
+++ Checksum_open_app/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+static string getOpenedAppFilepath()
+{
+    return System.Reflection.Assembly.GetEntryAssembly().Location;
+}
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(getOpenedAppFilepath());
+Console.WriteLine("Completed");
